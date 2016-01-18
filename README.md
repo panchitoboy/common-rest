670b57c2c9bed0ab5d28f03c0e52db1d550f144b
panchitoboy <panchitoboy@gmail.com>
2016-01-18 22:17:03 +0100

    Update jackson feature Update README.md Remove deltaspike

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Rest Web Service Util
 
-This library gives you two importants features when you are working with RestService and jax-rs.
+This library gives you two basic features when you are working with RestService and jax-rs.
 
 There are the result of working in angular projects where i use a lot of web services rest.
+
 
 ## ObjectMapperProvider
 
@@ -11,6 +12,9 @@ There are the result of working in angular projects where i use a lot of web ser
 
 ### Features:
 - DataFormat using full utc format
+
+Works with OffsetDateTime
+
 
 - Accept simple value as array
 
@@ -33,21 +37,11 @@ With the provider
 ## ValidationExceptionMapper
 
 ### Requires:
-- Nothing, but user Apache Delta Spike lastest version.
+- Nothing
 
 ### Features:
 - Default mapper for the EjbException in jax-rs applications. It takes the message in the exception as key and search it in resource bundle file. (By default: messages.properties)
 
-- Use Apache DeltaSpike to define a custom location of your messages file. 
-
 - Use the HttpServletRequest to get the locale of the request and send the correct message.
 
-- In the case that the cause is a ConstraintViolationException. Iterate all the messages and return an array of erros.
-
-## Basic Usage
-
-TO DO
-
-## Examples
-
-TO DO
+- In the case that the cause is a ConstraintViolationException. Iterate all the messages and return an array of errors.
--- src/main/java/com/github/panchitoboy/common/rest/ObjectMapperProvider.java
+++ src/main/java/com/github/panchitoboy/common/rest/ObjectMapperProvider.java
@@ -3,9 +3,8 @@ package com.github.panchitoboy.common.rest;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
@@ -15,15 +14,15 @@ import javax.ws.rs.ext.Provider;
 @Produces(MediaType.APPLICATION_JSON)
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
 
-    private final static DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
     private final ObjectMapper objectMapper;
 
     public ObjectMapperProvider() {
         this.objectMapper = new ObjectMapper();
-        df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        this.objectMapper.setDateFormat(df);
         this.objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         this.objectMapper.disable(MapperFeature.USE_GETTERS_AS_SETTERS);
+
+        this.objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        this.objectMapper.registerModule(new JavaTimeModule());
     }
 
     @Override
